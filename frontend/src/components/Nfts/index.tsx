16ae44b01530b0c1793d0f389583bbf4170ecf86
heronlancellot <heronlancellot@id.uff.br>
2023-04-22 19:18:04 -0300

    Feat: Index.tsx

--- frontend/src/components/Nfts/index.tsx
+++ frontend/src/components/Nfts/index.tsx
@@ -5,7 +5,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 
 import { NftOrder } from 'src/models/nft_orders';
 
@@ -89,31 +89,36 @@ export default function MediaNft() {
 
   return (
     <>
-    <Grid container spacing={2}>
-      {nftOrder.map((nftData, index) => (
-        <Grid item xs={12} sm={6} md={6} lg={4} key={index}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image={nftData.image}
-              title="Web3Dev Blockchain"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {nftData.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {nftData.description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+    <Box 
+      sx={{
+        marginTop: 4,
+      }}>
+      <Grid container spacing={2}>
+        {nftOrder.map((nftData, index) => (
+          <Grid item xs={12} sm={6} md={6} lg={4} key={index}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image={nftData.image}
+                title="Web3Dev Blockchain"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {nftData.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {nftData.description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
     </>
   );
 }
